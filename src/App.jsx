13606754651084295eea9e35518c023db51feebc
Emmanuel Zamora <emmanuel-zamora@split.io>
2023-06-28 01:45:01 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+import {logo } from './assets'
+
+const App = () => (
+    <div>
+      <img src={logo} alt="logo" style={{width: "100%"}}/>
+    </div>
+);
+
+export default App
